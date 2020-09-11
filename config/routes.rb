c3071b6a94576b1b73ae2b46e23315b9913a9bb5
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-11 14:27:49 -0400

    adding stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :flavors
+  resources :foods
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users, only: :create 
 end
